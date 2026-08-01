a7f5f8b449dda0f471e544c964e0ace9a3109cec
MaxMcNeil <72625668+MaxMcNeil@users.noreply.github.com>
2026-08-01 21:33:57 +0200

    Add files via upload

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,7 +22,6 @@ data "oci_identity_availability_domains" "ads" {
 data "oci_core_images" "ubuntu" {
   compartment_id           = var.compartment_ocid
   operating_system         = "Canonical Ubuntu"
-  operating_system_version = "22.04"
   shape                    = "VM.Standard.A1.Flex"
   sort_by                  = "TIMECREATED"
   sort_order                = "DESC"
